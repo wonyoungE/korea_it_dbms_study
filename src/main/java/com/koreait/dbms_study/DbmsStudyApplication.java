62eaee7dec21eb38d543cf072b0cbebfe88120e2
wonyoungE <error071312@gmail.com>
2025-06-17 22:43:57 +0900

    Init: first commit

---
+++ src/main/java/com/koreait/dbms_study/DbmsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.dbms_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koreait/dbms_study/DbmsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.dbms_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
